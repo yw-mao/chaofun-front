7fc2d158d6b61f6be2bed30b89f542566dd02eb6
yinzhecheng <yinzhecheng@yinzhechengdeMacBook-Pro.local>
2021-07-08 01:15:54 +0800

    u

--- src/components/chaofan/component/itemVideo.vue
+++ src/components/chaofan/component/itemVideo.vue
@@ -1,134 +1,141 @@
 <template>
- <div>  
-    <div :class="['item_video',{'phone_v': ISPHONE}]">
-        <div class="title">
-            {{item.title}}
-        </div>
-        <div :id="'video'+item.postId" class="video">
-            <video :class="[doHeight()]" :autoplay="true" webkit-playsinline='true' x5-video-player-type="h5"  playsinline='true' controls  :src="imgOrigin+item.video" alt=""></video>
-        </div>
+  <div>
+    <div :class="['item_video', { phone_v: ISPHONE }]">
+      <div class="title">
+        {{ item.title }}
+      </div>
+      <div :id="'video' + item.postId" class="video">
+        <video
+          :class="[doHeight()]"
+          :autoplay="true"
+          webkit-playsinline="true"
+          x5-video-player-type="h5"
+          playsinline="true"
+          controls
+          :src="imgOrigin + item.video"
+          alt=""
+        ></video>
+      </div>
     </div>
- </div>
+  </div>
 </template>
 
 <script>
-import * as api from '@/api/api'
- export default {
-   name: '',
-   data(){
-     return {
-         handle: ''
-     }
-   },
-   props: {
-       item: {
-           type: Object,
-           default(){
-               return {}
-           }
-       },
-       isDetail: {
-         type: Boolean,
-         default: false
-       },
-   },
-   components: {
-
-   },
-   created() {
-     console.log('879');
-     
-   },
-   mounted() {
-     if(document.getElementById('container')){
-       let self = this;
-        document.getElementById('container').addEventListener("scroll", self.handlerScroll);
-        window.addEventListener('unload', self.handlerScroll)
-     }
-     
-   },
-   destroyed() {
+import * as api from "@/api/api";
+export default {
+  name: "",
+  data() {
+    return {
+      handle: "",
+    };
+  },
+  props: {
+    item: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    isDetail: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  components: {},
+  created() {
+    console.log("879");
+  },
+  mounted() {
+    if (document.getElementById("container")) {
       let self = this;
-      if(document.getElementById('container')){
-        document.getElementById('container').removeEventListener('scroll', self.handlerScroll,false);
-      }
-   },
-   methods: {
-    handlerScroll(e){
-      console.log('879')
+      document
+        .getElementById("container")
+        .addEventListener("scroll", self.handlerScroll);
+    }
+  },
+  destroyed() {
+    let self = this;
+    if (document.getElementById("container")) {
+      document
+        .getElementById("container")
+        .removeEventListener("scroll", self.handlerScroll, false);
+    }
+  },
+  methods: {
+    handlerScroll(e) {
+      console.log("879");
       console.log(e.target);
       console.log(e.target.offsetTop);
-      var el = document.getElementById('video'+this.item.postId);
-      console.log(this.item)
-      if(el){
+      var el = document.getElementById("video" + this.item.postId);
+      console.log(this.item);
+      if (el) {
         var top = el.getBoundingClientRect().top;
-        if(top<-50){
-          if(!this.isDetail){
-            
-            this.$emit('toPause','',this.item,0)
+        if (top < -50) {
+          if (!this.isDetail) {
+            this.$emit("toPause", "", this.item, 0);
           }
         }
-      }else{
-        console.log('取消滚动监听------------------')
+      } else {
+        console.log("取消滚动监听------------------");
         let self = this;
-        document.getElementById('container').removeEventListener('scroll', self.handlerScroll,false);
+        document
+          .getElementById("container")
+          .removeEventListener("scroll", self.handlerScroll, false);
       }
-      
-      console.log('距离顶部',top)
-    },
-    unloadHandler(e){
 
+      console.log("距离顶部", top);
     },
-    doHeight(){
-      if(this.isDetail){
-        if(this.ISPHONE){
-          return 'video3';
-        }else{
-          return 'video2';
+    unloadHandler(e) {},
+    doHeight() {
+      if (this.isDetail) {
+        if (this.ISPHONE) {
+          return "video3";
+        } else {
+          return "video2";
         }
-      }else{
-        return ''
+      } else {
+        return "";
       }
     },
-    toUrls(item,params){
-       this.postBehavior(item.postId,'jump');
-       this.toUrl(params)
+    toUrls(item, params) {
+      this.postBehavior(item.postId, "jump");
+      this.toUrl(params);
     },
-   }
- }
+  },
+};
 </script>
 
 <style type='text/scss' lang='scss' scoped>
-.item_video{
-      .title{
-        padding: 0 0 10px 0;
-        font-size: 16px;
-        font-weight: 600;
-      }
-      .video{
-        text-align: center;
-        // width: 640px;
-        max-height: 512px;
-        overflow-y: hidden;
-        video{
-          width: 100%;
-          height: 320px;
-          background: #000;
-          margin: 0 auto;
-        }
-        .video2{
-            height: 440px;
-        }
-        .video3{
-          height: 290px;
-        }
-      }
+.item_video {
+  .title {
+    padding: 0 0 10px 0;
+    font-size: 16px;
+    font-weight: 600;
+  }
+  .video {
+    text-align: center;
+    // width: 640px;
+    max-height: 512px;
+    overflow-y: hidden;
+    video {
+      width: 100%;
+      height: 320px;
+      background: #000;
+      margin: 0 auto;
     }
-  .phone_v{
-    .video{
-      video{
-        height: 230px;
-      }
+    .video2 {
+      height: 440px;
+    }
+    .video3 {
+      height: 290px;
     }
   }
+}
+.phone_v {
+  .video {
+    video {
+      height: 230px;
+    }
+  }
+}
 </style>
