27c010f760a3f8460dacf080aea410b8a9312c7c
Yue Zhang <zhangyue.zhangyue@alibaba-inc.com>
2021-10-19 20:52:15 +0800

    chore: 鸣谢

--- src/views/chaofun-webview/thx/thx.vue
+++ src/views/chaofun-webview/thx/thx.vue
@@ -2,7 +2,7 @@
   <div>
   <div>感谢以下给炒饭做过贡献的饭友</div>
   <div @click="toUser(53906)">感谢 @猫 在导航栏样式、发帖，评论&工程结构上做的优化</div>
-  <div @click="toUser(56018)">感谢 @阿鲤 在图片处理做的贡献</div>
+  <div @click="toUser(56018)">感谢 @阿鲤 在图片处理，水印处理上做的贡献</div>
   </div>
 </template>
 
