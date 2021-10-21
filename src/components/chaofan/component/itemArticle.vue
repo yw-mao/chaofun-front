f12c129c657196b47162fdff64bf7b78c80a84b3
猫爪子 <gfel@live.cn>
2021-10-21 15:31:48 +0800

    feat: 隐藏部分功能

--- src/components/chaofan/component/itemArticle.vue
+++ src/components/chaofan/component/itemArticle.vue
@@ -122,36 +122,36 @@ export default {
 <style type='text/scss' lang='scss' scoped>
 .item_article {
   padding-bottom: 20px !important;
-      .title{
-        padding: 0 0 10px 0;
-        font-size: 16px;
-        font-weight: 600;
-      }
-      .article{
-        padding-bottom: 10px;
-        max-height: 265px;
-        overflow: hidden;
-        position: relative;
-        .show_more{
-          position: absolute;
-          top: 200px;
-          left: 0;
-          width: 100%;
-          text-align: center;
-          height: 70px;
-          line-height: 90px;
-          z-index: 1;
-          background: linear-gradient(-180deg,rgba(255,255,255,0) 0%,#fff 50%);
-        }
-      }
-      .article_phone{
-        max-height: 160px;
-        .show_more{
-          top: 110px;
-          height: 50px;
-          line-height: 60px;
-        }
-      }
+  .title{
+    padding: 0 0 10px 0;
+    font-size: 16px;
+    font-weight: 600;
+  }
+  .article{
+    padding-bottom: 10px;
+    max-height: 265px;
+    overflow: hidden;
+    position: relative;
+    .show_more{
+      position: absolute;
+      top: 200px;
+      left: 0;
+      width: 100%;
+      text-align: center;
+      height: 70px;
+      line-height: 90px;
+      z-index: 1;
+      background: linear-gradient(-180deg,rgba(255,255,255,0) 0%,#fff 50%);
+    }
+  }
+  .article_phone{
+    max-height: 160px;
+    .show_more{
+      top: 110px;
+      height: 50px;
+      line-height: 60px;
+    }
+  }
 }
 .item .item_article .article_phone{
     position: relative;
