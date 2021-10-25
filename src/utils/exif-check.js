a6e73823ed0ac924bf857dd8dac943ee4b1b0e4e
猫爪子 <gfel@live.cn>
2021-10-25 12:12:04 +0800

    perf: 优化文件格式与后缀名不符合跳过EXIF检测

--- src/utils/exif-check.js
+++ src/utils/exif-check.js
@@ -15,64 +15,68 @@ export default (file, $createElement) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function() {
-      const exif = piexif.load(reader.result);
+      try {
+        const exif = piexif.load(reader.result);
 
-      if (Object.keys(exif.GPS).length > 0) {
-        console.log(exif.GPS);
-        console.log(piexif.GPSIFD.GPSLatitude);
-        const lng = exif.GPS[piexif.GPSIFD.GPSLongitude];
-        const lngRef = exif.GPS[piexif.GPSIFD.GPSLongitudeRef];
-        const lat = exif.GPS[piexif.GPSIFD.GPSLatitude];
-        const latRef = exif.GPS[piexif.GPSIFD.GPSLongitudeRef];
-        // 检测成功，提示
-        if (lat && latRef && lng && lngRef) {
-          const realLng = piexif.GPSHelper.dmsRationalToDeg(lng, lngRef);
-          const realLat = piexif.GPSHelper.dmsRationalToDeg(lat, latRef);
-          const h = $createElement;
-          const content = h('div', null, [
-            h('div', {
-              class: 'msg-lnglat-box'
-            }, [
-              h('p', null, [
-                '经度：',
-                h('strong', null, realLng),
+        if (Object.keys(exif.GPS).length > 0) {
+          console.log(exif.GPS);
+          console.log(piexif.GPSIFD.GPSLatitude);
+          const lng = exif.GPS[piexif.GPSIFD.GPSLongitude];
+          const lngRef = exif.GPS[piexif.GPSIFD.GPSLongitudeRef];
+          const lat = exif.GPS[piexif.GPSIFD.GPSLatitude];
+          const latRef = exif.GPS[piexif.GPSIFD.GPSLongitudeRef];
+          // 检测成功，提示
+          if (lat && latRef && lng && lngRef) {
+            const realLng = piexif.GPSHelper.dmsRationalToDeg(lng, lngRef);
+            const realLat = piexif.GPSHelper.dmsRationalToDeg(lat, latRef);
+            const h = $createElement;
+            const content = h('div', null, [
+              h('div', {
+                class: 'msg-lnglat-box'
+              }, [
+                h('p', null, [
+                  '经度：',
+                  h('strong', null, realLng),
+                ]),
+                h('p', null, [
+                  '纬度：',
+                  h('strong', null, realLat),
+                ]),
               ]),
-              h('p', null, [
-                '纬度：',
-                h('strong', null, realLat),
-              ]),
-            ]),
-            h('p', null, '请注意保护您的个人隐私。在您选择之前，我们将不会上传照片'),
-            h('p', null, '如需取消上传，请点击右上角关闭按钮或空白处！'),
-          ]);
+              h('p', null, '请注意保护您的个人隐私。在您选择之前，我们将不会上传照片'),
+              h('p', null, '如需取消上传，请点击右上角关闭按钮或空白处！'),
+            ]);
 
-          let result = false;
-          MessageBox({
-            title: '您的照片含有GPS定位信息，是否需要清除？',
-            message: content,
-            showCancelButton: true,
-            confirmButtonText: '清除并上传',
-            cancelButtonText: '保留并上传',
-            distinguishCancelAndClose: true,
-            beforeClose: (action, instance, done) => {
-              if (action === 'confirm') {
-                result = dataURLtoFile(piexif.remove(reader.result), file.name);
-              } else if (action === 'cancel') {
-                result = true;
-              } else if (action === 'close') {
-                result = false;
+            let result = false;
+            MessageBox({
+              title: '您的照片含有GPS定位信息，是否需要清除？',
+              message: content,
+              showCancelButton: true,
+              confirmButtonText: '清除并上传',
+              cancelButtonText: '保留并上传',
+              distinguishCancelAndClose: true,
+              beforeClose: (action, instance, done) => {
+                if (action === 'confirm') {
+                  result = dataURLtoFile(piexif.remove(reader.result), file.name);
+                } else if (action === 'cancel') {
+                  result = true;
+                } else if (action === 'close') {
+                  result = false;
+                }
+                done();
               }
-              done();
-            }
-          }).then(() => {
-            resolve.log(result);
-          }).catch(() => {
-            resolve(result);    
-          });
+            }).then(() => {
+              resolve.log(result);
+            }).catch(() => {
+              resolve(result);    
+            });
+          }
+          return;
         }
-        return;
+      } catch (error) {
+        console.log(error);
       }
-
+      
       // 通过检测
       resolve(true);
     };
