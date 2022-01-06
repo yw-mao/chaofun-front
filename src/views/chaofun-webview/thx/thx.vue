c3b882dddf358cb2bd5e35e7b6dc14a725287383
Yue Zhang <cijianzy@gmail.com>
2022-01-06 17:56:18 +0800

    fix: 最新贡献靠前

--- src/views/chaofun-webview/thx/thx.vue
+++ src/views/chaofun-webview/thx/thx.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div>感谢以下给炒饭做过贡献的饭友</div>
+    <div @click="toUser(50777)">感谢 @匿名用户cf Web 帖子评论交互，发帖上做出的贡献</div>
     <div @click="toUser(53906)">感谢 @猫 在导航栏样式、发帖，评论&工程结构上做的优化</div>
     <div @click="toUser(56018)">感谢 @阿鲤 在图片处理，水印处理上做的贡献</div>
-    <div @click="toUser(50777)">感谢 @匿名用户cf Web 帖子交互上做出的贡献</div>
   </div>
 </template>
 
