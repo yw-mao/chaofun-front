e595ab70e67e8300fc3abf0e93e7c40904bad60b
猫爪子 <gfel@live.cn>
2022-01-07 03:16:48 +0800

    feat: 增加用户名显示

--- src/views/chao/components/item.vue
+++ src/views/chao/components/item.vue
@@ -5,7 +5,16 @@
         :src="`${imgOrigin}${item.imageName}?x-oss-process=image/resize,w_236`"
       />
     </div>
-    
+    <div class="author">
+      <router-link to="/setting">
+        <span>
+          <img :src="`${imgOrigin}${item.userInfo.icon}?x-oss-process=image/resize,h_80`" alt="">
+        </span>
+        <div class="username">
+          {{item.userInfo.userName}}
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 <script>
@@ -37,6 +46,38 @@ export default {
     overflow: hidden;
     img {
       display: block;
+    }
+  }
+  .author {
+    padding: 8px 6px 0;
+    a {
+      display: flex;
+      flex-direction: row;
+      line-height: 32px;
+      color: #111;
+      margin: 0 -3px;
+
+      span {
+        display: block;
+        margin: 0 3px;
+        background: #f4f4f4;
+        border-radius: 50%;
+        width: 32px;
+        height: 32px;
+        overflow: hidden;
+        line-height: 32px;
+        img {
+          display: block;
+          color: transparent;
+          mix-blend-mode: multiply;
+          width: 100%;
+         height: 100%;
+        }
+      }
+      .username {
+        margin: 0 3px;
+        flex: 1 1 auto;
+      }
     }
   }
 }
