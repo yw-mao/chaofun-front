b10a598238584eb76cf3415a2838a8ab3022698d
chaofunchengfeng <chaofunchengfeng@qq.com>
2022-01-24 12:47:22 +0800

    fix：表情输入框的层叠显示问题

--- src/views/list/articleDetail.vue
+++ src/views/list/articleDetail.vue
@@ -93,7 +93,7 @@
                             <img style="vertical-align:middle;margin-right:10px;cursor:pointer;" src="../../assets/images/icon/choose.png" alt="">
                             
                             </el-upload>
-                            <div class="icons">
+                            <div class="icons" style="z-index: 1;">
                               <img @click="showIcons" src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=105646479,4120396531&fm=26&gp=0.jpg" alt="">
                               <div  class="emoji">
                                 <span v-for="(item,index) in icons" @click="chooseEmoji(item)" :key="index">{{item}}</span>
@@ -1071,7 +1071,7 @@ queryChildren (parent, list) {
 }
 .dialog_top{
     position: absolute;
-    z-index: 1;
+    z-index: 2;
     // width: calc(80% - 20px);
     width: 100%;
     // left: 10%;
