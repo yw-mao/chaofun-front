78201712c18e694839c3c3a5a3159798fc5f8e0a
chaofunchengfeng <chaofunchengfeng@qq.com>
2022-02-08 00:16:49 +0800

    红包领取样式修改

--- src/views/chaofun-webview/fbi/RedPacket.vue
+++ src/views/chaofun-webview/fbi/RedPacket.vue
@@ -1,94 +1,128 @@
 <template>
   <div>
+
     <div style="position: fixed; top: 0; right: 0; text-align: right">
       <div>
-        <el-button @click="create"> 创建口令红包 </el-button>
+        <el-button @click="create"> 创建口令红包</el-button>
       </div>
       <div>
-        <el-button @click="history" style="right: 0"> 红包历史 </el-button>
+        <el-button style="right: 0" @click="history"> 红包历史</el-button>
       </div>
     </div>
-    <div v-if="showDetail" style="align-items: center; text-align:center">
-      <div v-if="redPacket" style="padding-top: 100px;">
-        {{redPacket.sendUser.userName}} 发出的 FBi 红包
-      </div>
-      <div style="padding-top: 10px; font-size: 20px; font-weight: bold">
-        {{redPacket.blessing}}
-      </div>
-      <p v-if="this.redPacket.isOpened">
-        你获得 <span style="font-weight: bold; font-size: 20px; color: #C3A16C;">{{this.redPacket.got}} </span> FBi
-      </p>
-      <p v-else-if="redPacket.isEnd">
-        红包已经结束
-      </p>
-      <p>
-        总额 {{redPacket.fbiTotal}} FBi, 领取 {{redPacket.userGot}} / {{redPacket.userTotal}}
-      </p>
-    </div>
-    <div v-else-if="redPacket" style="align-items: center; text-align:center">
 
-      <div>
-        <div style="background:#b64340; width: 300px; min-width: 300px; max-width: 300px;height: 400px; min-height: 400px;max-height: 400px;
-border-radius: 10px; margin: 100px auto 0px auto;">
-          <div style="background: #be4645; width: 300px;height: 150px;   border-radius: 10px; position: relative;"/>
-          <div style="background: #be4645; border-radius: 300px;width: 300px;height: 300px;  position: relative;top:-150px;"/>
-          <div style="background: #e6cea0; border-radius: 300px;width: 60px;height: 60px;  position: relative;top:-175px; left: 120px; font-size: 42px; cursor: pointer;"
-               @click.stop="openRedPacket">開
-          </div>
-          <div style="font-size: 14px;color: #fce5b9;cursor: pointer;position: relative;top:-140px; " @click.stop="showDetail=true">查看领取详情 ></div>
-        </div>
-      </div>
-
-      <div style="position: relative;top: -400px;">
-        <div style="border-radius: 60px; width: 60px;height: 60px; margin: 20px auto 10px auto; background: #fff; ">
-          <img :src="imgOrigin +redPacket.sendUser.icon +'?x-oss-process=image/resize,h_60'" alt=""/>
-        </div>
-        <div style="font-size: 20px;color: #fce5b9;cursor: pointer; " @click.stop="toUser(redPacket.sendUser.userId)">{{ redPacket.sendUser.userName }}</div>
-        <div style="font-size: 30px;color: #fff; margin-top: 50px;"> {{ redPacket.blessing }}</div>
-      </div>
-
-<!--      <div style="padding-top: 100px;">-->
-<!--        {{redPacket.sendUser.userName}} 发出的 FBi 红包-->
-<!--      </div>-->
-<!--      <div style="padding-top: 10px; font-size: 20px; font-weight: bold">-->
-<!--        {{redPacket.blessing}}-->
-<!--      </div>-->
-<!--      <el-button style="margin-top: 10px" @click="openRedPacket">打开红包</el-button>-->
-    </div>
-    <div v-else style="align-items: center; text-align:center">
+    <div style="align-items: center; text-align:center">
       <div style="padding-top: 100px; font-size: 20px;">
         输入口令领取 FBi 红包
       </div>
-      <el-input v-model="input" style="margin-top: 10px; padding-left: 20%; padding-right: 20%"></el-input>
-      <el-button style="margin-top: 10px" @click="inputEnter">确定</el-button>
+      <div style="margin-top: 10px">
+        <el-input v-model="password" :maxlength=20 style="width: 200px;"></el-input>
+      </div>
+      <div style="margin-top: 10px">
+        <el-button @click="inputEnter">确定</el-button>
+      </div>
     </div>
+
+    <!-- dialog -->
+    <el-dialog v-if="showDialog" :append-to-body="true" :modal-append-to-body="false" :visible.sync="showDialog" center custom-class="el-dialog-custom"
+               top="10px" width="300px" @close="dialogClosed">
+
+      <!-- title -->
+      <div slot="title" style="font-size: 20px;color: #fff;">FBi 红包</div>
+
+      <!-- body -->
+      <div style="width: 300px; height: 500px;background: #808080;">
+
+        <div style="width: 300px;height: 100px;position: absolute;top: 60px;">
+          <transition name="el-zoom-in-bottom">
+            <div v-show="redPacket.isOpened" class="transition-box" style="width: 300px;height: 100px;background: #b64340; border-radius:50% / 100% 100% 0 0;"/>
+          </transition>
+        </div>
+
+        <!-- 用户信息 -->
+        <div style="width: 300px; height: 400px;background:#be4645;  border-radius: 0px 0px 10px 10px; margin: 0px;position: absolute;top: 160px;">
+          <div style="width: 300px;height: 100px;background: #b64340; border-radius:50% / 0 0 100% 100%; "/>
+          <div style="width: 300px;height: 150px; position: relative;top:0px;">
+            <div style="width: 40px;height: 40px; border-radius: 60px; margin: 20px auto 10px auto; background: #fff; ">
+              <img :src="imgOrigin +redPacket.sendUser.icon +'?x-oss-process=image/resize,h_40'" alt="" style="width: 40px;height: 40px; border-radius: 60px;"/>
+            </div>
+            <div style="font-size: 20px;color: #fce5b9;cursor: pointer; " @click.stop="toUser(redPacket.sendUser.userId)">{{ redPacket.sendUser.userName }}</div>
+            <div style="font-size: 30px;color: #fff; margin-top: 30px;"> {{ redPacket.blessing }}</div>
+          </div>
+        </div>
+
+        <!-- 标记本人红包 -->
+        <div v-if="redPacket.isOwner" style="width: 20px;height: 20px;background:#cba345;color: #fff;font-size: 14px; border-radius: 5px; align-items: center;text-align: center;
+               position: absolute;top:162px;right: 2px;">我
+        </div>
+
+        <!-- 动态信息 -->
+        <div v-if="redPacket.isOpened" style="width: 300px; font-weight: lighter; font-size: 40px; color: #fce5b9; position: absolute;top: 150px;">{{ redPacket.got }} Fbi</div>
+        <div v-else-if="redPacket.isEnd" style="width: 300px; font-weight: lighter; font-size: 28px; color: #fce5b9; position: absolute;top: 180px;">来晚一步，领完啦~</div>
+        <div v-else style="width: 300px;height: 80px; position: absolute;top:180px;">
+          <div style="width: 80px;height: 80px;background: #e6cea0; border-radius: 300px;  position: relative; left: 110px; font-size: 58px; cursor: pointer;"
+               @click.stop="openRedPacket">開
+          </div>
+        </div>
+
+        <!-- 领取详情 -->
+        <div v-if="0 != redPacket.userTotal" style="width: 300px;height: 20px; position: absolute; top:520px;">
+          <div style="width: 300px;height: 20px;font-size: 16px;color: #fff;">已领取 {{ redPacket.userGot }} / {{ redPacket.userTotal }} 个，共 {{
+              redPacket.fbiTotal - redPacket.fbiRemain
+            }} / {{ redPacket.fbiTotal }} FBi
+          </div>
+        </div>
+
+      </div>
+
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
 import * as api from '@/api/api'
-import {getUserInfo} from "../../../api/api";
+
 export default {
   name: "RedPacket",
+  components: {},
+
   data() {
     return {
-      input: '',
-      showDetail: false,
+      password: '',
       redPacket: null,
-      fbi: 0,
+      showDialog: false,
+
     }
   },
   created() {
-    this.password= this.$route.query.password;
+
+  },
+  mounted() {
+
+    let passwd = this.$route.query.password;
+    if (!passwd) {
+      return;
+    }
+    if (passwd.length > 20) {
+      passwd = passwd.substr(0, 20);
+    }
+    this.password = passwd;
     if (this.password) {
       this.getRedPacket()
     }
-  },
-  mounted() {
 
   },
 
   methods: {
+
+    reinitData() {
+      this.showDialog = false;
+      this.redPacket = null;
+    },
+
+    dialogClosed() {
+      this.reinitData();
+    },
 
     toUser(userId) {
       if (userId) {
@@ -101,14 +135,24 @@ export default {
     },
 
     getRedPacket() {
-      api.getByPath('/api/v0/red_packet/get', {'password': this.password, 'type': 'password'}).then( (res) => {
-        if (res.success && res.data !== null) {
+      api.getByPath('/api/v0/red_packet/get', {'password': this.password, 'type': 'password'}).then((res) => {
+        // this.handleRedPacketData(res);
+        if (res.success && res.data != null) {
+
           this.redPacket = res.data;
-          if (this.redPacket.isOpened || this.redPacket.isEnd) {
-            this.showDetail = true;
+
+          // 兼容祝福语长度
+          if (this.redPacket.blessing && this.redPacket.blessing.length > 14) {
+            this.redPacket.blessing = this.redPacket.blessing.substr(0, 14);
           }
+
+          //
+          this.showDialog = true;
+
         } else {
-          this.redPacket = null
+
+          this.reinitData();
+
           if (res.errorMessage) {
             this.$toast(res.errorMessage);
           } else {
@@ -119,25 +163,32 @@ export default {
     },
 
     openRedPacket() {
-      api.getByPath('/api/v0/red_packet/open', {'password': this.password, 'type': 'password'}).then( (res) => {
+      api.getByPath('/api/v0/red_packet/open', {'password': this.password, 'type': 'password'}).then((res) => {
+        // this.handleRedPacketData(res);
         if (res.success && res.data != null) {
+
           this.redPacket = res.data;
-          if (this.redPacket.isOpened || this.redPacket.isEnd) {
-            this.showDetail = true;
+
+          // 兼容祝福语长度
+          if (this.redPacket.blessing && this.redPacket.blessing.length > 14) {
+            this.redPacket.blessing = this.redPacket.blessing.substr(0, 14);
           }
+
+          //
+          this.showDialog = true;
+
         } else {
-          this.redPacket = null
-          if (res.errorMessage) {
-            this.$toast(res.errorMessage);
-          } else {
-            this.$toast('没有这个红包');
-          }
+          // 先这样处理
+          this.getRedPacket();
         }
-      }).bind(this);
+      });
+    },
+
+    handleRedPacketData(res) {
+
     },
 
     inputEnter() {
-      this.password = this.input;
       this.getRedPacket();
     },
 
@@ -154,6 +205,27 @@ export default {
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+/deep/ .el-dialog-custom {
+  box-shadow: unset;
+  border: none;
+
+  .el-dialog__header {
+    background: #808080;
+    height: 60px;
+
+    .el-dialog__close {
+      color: #aaa;
+      font-size: 28px;
+    }
+  }
+
+  .el-dialog__body {
+    padding: 0px;
+    align-items: center;
+    text-align: center;
+  }
+}
 
 </style>
