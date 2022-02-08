cb638d15a516264cf6f021a397f75f2ea535fc88
chaofunchengfeng <chaofunchengfeng@qq.com>
2022-02-08 23:00:30 +0800

    ListComment重写toTop按钮

--- src/views/list/ListComment.vue
+++ src/views/list/ListComment.vue
@@ -2,8 +2,11 @@
   <div>
     <div v-for="(item, index) in lists" :key="index" style="margin-bottom: 20px;padding-bottom:15px;
     border: #e0e0e0 solid 3px;border-radius: 10px; background: #f1f1f1">
-      <ListItem :lists="[item.post]" :is-show-to-top-button="false"></ListItem>
+      <ListItem :is-show-to-top-button="false" :lists="[item.post]"></ListItem>
       <commentItem :treeData="[item]" style="margin-right: 10px;"></commentItem>
+    </div>
+    <div :class="[ISPHONE ? 'scroll_to_top_phone' : 'scroll_to_top']" @click="toTop">
+      <img alt="" src="../../assets/images/icon/to_top.png"/>
     </div>
   </div>
 </template>
@@ -35,9 +38,42 @@ export default {
   destroyed() {
 
   },
-  methods: {},
+  methods: {
+
+    toTop() {
+      this.$(".infinite-list").animate({scrollTop: 0}, 200);
+    },
+
+  },
 };
 </script>
 
 <style lang='scss' scoped type='text/scss'>
+.scroll_to_top_phone {
+  position: fixed;
+  width: 40px;
+  height: 40px;
+  right: 10px;
+  bottom: 65px;
+  cursor: pointer;
+  z-index: 1;
+
+  img {
+    width: 100%;
+  }
+}
+
+.scroll_to_top {
+  position: fixed;
+  width: 40px;
+  height: 40px;
+  right: 80px;
+  bottom: 120px;
+  cursor: pointer;
+  z-index: 1;
+
+  img {
+    width: 100%;
+  }
+}
 </style>
