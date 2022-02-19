01ae2f937ddf1f0d4e9590ba4593fada6358a6a9
uvd <386180127@qq.com>
2022-02-19 10:46:25 +0800

    Update vue.config.js
    前端静态资源配置cdn地址

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ const port = process.env.port || process.env.npm_config_port || 8099
 const isProduction = process.env.NODE_ENV === 'production';
 
 // 线上打包路径，请根据项目实际线上情况
-const BASE_URL = process.env.NODE_ENV === "production" ? "/" : "/";
+const BASE_URL = process.env.NODE_ENV === "production" ? "https://s.chao.fun/" : "/";
 
 const fs = require('fs');
 
