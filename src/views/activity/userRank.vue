8f11b962588742ba12814b6f1f1893d54e14a7a6
cijianzy <cijianzy@gmail.com>
2022-04-03 22:00:01 +0800

    fix:  优化

--- src/views/activity/userRank.vue
+++ src/views/activity/userRank.vue
@@ -8,7 +8,7 @@
         </div>
         <div @click="toUser(item)" v-for="(item,index) in pagedata" :key="index" class="item">
             <div class="left">
-                <img :src="imgOrigin+item.icon + '?x-oss-process=image/resize,h_80/format,webp/quality,q_75'" alt="">
+                <img style="object-fit: cover;" :src="imgOrigin+item.icon + '?x-oss-process=image/resize,h_80/format,webp/quality,q_75'" alt="">
                 <div class="info">
                     <div class="title">{{item.userName}}</div>
                     <p v-if="item.desc" class="desc">{{item.desc}}</p>
