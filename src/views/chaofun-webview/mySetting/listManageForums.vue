15df761fb31d8515d1791376e29fd24efb8883ff
chaofunchengfeng <chaofunchengfeng@qq.com>
2022-05-24 15:44:07 +0800

    feat:辞任版主

--- src/views/chaofun-webview/mySetting/listManageForums.vue
+++ src/views/chaofun-webview/mySetting/listManageForums.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
     <div v-for="(item,index) in manageForumsList" :key="index"
-         class="item" @click="toForum(item.id)">
+         class="item">
 
-      <span style="margin: auto 0;">
+      <span style="margin: auto 0; cursor: pointer;" @click="toForum(item.id)">
         <img :src="imgOrigin + item.imageName+ '?x-oss-process=image/resize,h_40/format,webp/quality,q_75'" alt=""
              style="width: 40px;height: 40px;border-radius: 100%;" />
       </span>
 
-      <span style="margin: auto 5px;">
-        <div style="font-size: 16px;"> {{ item.name }} </div>
-        <div style="font-size: 12px;color: #999;">帖子数量： {{ item.posts }}</div>
+      <span style="margin: auto 5px;cursor: pointer;" @click="toForum(item.id)">
+        <span style="font-size: 16px;display: block;"> {{ item.name }} </span>
+        <span style="font-size: 12px;color: #999;display: block;">帖子数量： {{ item.posts }}</span>
       </span>
 
+      <div style="flex: 1;text-align: right;">
+        <el-popconfirm :title="`是否确定辞任【${item.name}】版主？`" @onConfirm="resign(item)">
+          <el-button slot="reference" type="warning">辞任</el-button>
+        </el-popconfirm>
+      </div>
+
     </div>
+
+    <div v-if="manageForumsList.length===0" style="color: #999;text-align: center;">暂未担任版主~</div>
+
   </div>
 </template>
 
 <script>
 
+import * as api from "@/api/api";
 import { listManageForums } from "@/api/api";
 
 export default {
@@ -29,16 +39,27 @@ export default {
     };
   },
   mounted() {
-    listManageForums().then(res => {
-      if (res.success) {
-        this.manageForumsList = res.data;
-      }
-    });
+    this.getList();
   },
   methods: {
     toForum(forumId) {
       window.open("/f/" + forumId, "_blank");
+    },
+
+    getList() {
+      listManageForums().then(res => {
+        if (res.success) {
+          this.manageForumsList = res.data;
+        }
+      });
+    },
+
+    resign(item) {
+      api.forumRemoveMod({ forumId: item.id, userId: this.$store.state.user.userInfo.userId }).then(() => {
+        this.getList();
+      });
     }
+
   }
 };
 </script>
@@ -46,13 +67,17 @@ export default {
 <style lang="scss" scoped>
 .item {
   display: flex;
+  align-items: center;
   padding: 5px 10px;
   border-top: 1px solid #eee;
   border-bottom: 1px solid #eee;
-  cursor: pointer;
 
   &:hover {
     background: #f9f9f9;
   }
+}
+
+::v-deep.el-button--medium {
+  padding: 6px 10px;
 }
 </style>
