52c2d6e8a3e83e26bd6091df08304fe4e0704095
cijianzy <cijianzy@gmail.com>
2022-07-02 12:16:27 +0800

    fix: 优化

--- src/views/tuxun/TXHome.vue
+++ src/views/tuxun/TXHome.vue
@@ -3,7 +3,7 @@
     <div :class="[{'im-view': !ISPHONE}, {'im-view-phone': ISPHONE}]">
       <div id="viewer" v-if="this.contentType === 'panorama'" style="width: 100%; height: 100%"></div>
       <img v-if="this.image && this.contentType === 'image'" v-viewer="{inline: false}" :data-source="imgOrigin+ this.image" style=" width: 100%;height: 100%;object-fit: contain;"  :src="imgOrigin+ this.image" alt=""></img>
-      <div v-if="status === 'rank'" style="position: absolute; width: 100%; height: 100%; background: white; opacity: 80% ">
+      <div v-if="status === 'rank'" style="position: absolute; width: 100%; height: 100%; background: white; opacity: 80%; overflow:auto;">
         <div style="padding-top: 40px; font-weight: bold; font-size: 20px;">排行榜:</div>
         <div v-if="this.rank" style="padding-top: 10px; font-weight: bold; font-size: 20px">你的本场次排名:{{this.rank}}</div>
         <div v-for="item in this.ranks" class="item">
