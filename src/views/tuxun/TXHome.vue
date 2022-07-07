23b5c0e0daa134ba8ef7852b31125227e4a8d713
cijianzy <cijianzy@gmail.com>
2022-07-07 13:40:48 +0800

    fix: 优化一下

--- src/views/tuxun/TXHome.vue
+++ src/views/tuxun/TXHome.vue
@@ -80,7 +80,7 @@
       <el-button size="mini" @click="toForum"> 社区讨论 </el-button>
       <el-button v-if="!this.isMaps" size="mini"  @click="toRank"> 积分排行 </el-button>
       <el-button v-if="!this.isMaps" size="mini"  @click="toSend"> 发送弹幕 </el-button>
-      <el-button v-if="!this.isMaps" size="mini"  @click="toHome"> 排位赛 </el-button>
+      <el-button v-if="this.isMaps" size="mini"  @click="toHome"> 排位赛 </el-button>
       <el-button size="mini"  @click="toMaps"> 训练赛 </el-button>
       <el-button size="mini"  @click="toReport"> 坏题反馈 </el-button>
     </div>
