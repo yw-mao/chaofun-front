7afda15f8eef26413f2564b8998e8529aaebab48
chaofunchengfeng <chaofunchengfeng@qq.com>
2022-07-14 17:18:46 +0800

    fix:微调图寻排行榜样式

--- src/views/tuxun/rank.vue
+++ src/views/tuxun/rank.vue
@@ -1,136 +1,120 @@
 <template>
-  <div class="container">
-    <div class="content">
-        <div class="nav">
-            <img src="../chaofun-webview/assets/images/rank.png" alt="">
-            <span>Top 100（前三名FBi奖励）</span>
-        </div>
-        <div @click="toUser(item)" v-for="(item,index) in pagedata" :key="index" class="item">
-          <div class="title">{{item.rank}}.</div>
-          <div class="left">
-                <img :src="imgOrigin+item.userAO.icon + '?x-oss-process=image/resize,h_80/format,webp/quality,q_75'" alt="">
-                <div class="info">
-                    <div class="title">{{item.userAO.userName}}</div>
-<!--                    <p v-if="item.userAO.desc" class="desc">{{item.userAO.desc}}</p>-->
-                </div>
-            </div>
-            <div class="right"> 积分
-                <p> {{item.rating}} </p>
-            </div>
-        </div>
+  <div style="margin: 0 auto;width: 640px;max-width: 100%;padding: 10px;">
+    <div style="display: flex;justify-content: center; align-items: center;">
+      <img alt="" src="../chaofun-webview/assets/images/rank.png">
+      <span style="font-size: 18px;">Top 100（前三名FBi奖励）</span>
     </div>
-    
+    <div style="display: flex;justify-content: space-around;position: relative;margin-top: 10px;">
+
+      <!-- 第2名 -->
+      <div
+        class="podium" style="position: relative;top: 30px;"
+        @click="toUser(pagedata[1])">
+        <span style="display: block;color: #C0C0C0;font-weight: bold;font-size: 120%;">{{ pagedata[1].rating }}</span>
+        <img :src="imgOrigin+pagedata[1].userAO.icon + '?x-oss-process=image/resize,h_100/format,webp/quality,q_75'"
+             alt=""
+             style="width: 50px; height: 50px; border-radius: 100%;border: 2px solid #C0C0C0;">
+        <span
+          style="display: block;color: #C0C0C0;font-weight: bold;font-size: 120%;">{{ pagedata[1].userAO.userName
+          }}</span>
+      </div>
+
+      <!-- 第1名 -->
+      <div
+        class="podium" style="position: relative;"
+        @click="toUser(pagedata[0])">
+        <span style="display: block;color: #FFD700;font-weight: bold;font-size: 120%;">{{ pagedata[0].rating }}</span>
+        <img :src="imgOrigin+pagedata[0].userAO.icon + '?x-oss-process=image/resize,h_120/format,webp/quality,q_75'"
+             alt=""
+             style="width: 60px; height: 60px; border-radius: 100%;border: 2px solid #FFD700;">
+        <span
+          style="display: block;color: #FFD700;font-weight: bold;font-size: 120%;">{{ pagedata[0].userAO.userName
+          }}</span>
+      </div>
+
+      <!-- 第3名 -->
+      <div
+        class="podium" style="position: relative;top: 30px;"
+        @click="toUser(pagedata[2])">
+        <span style="display: block;color: #C47222;font-weight: bold;font-size: 120%;">{{ pagedata[2].rating }}</span>
+        <img :src="imgOrigin+pagedata[2].userAO.icon + '?x-oss-process=image/resize,h_100/format,webp/quality,q_75'"
+             alt=""
+             style="width: 50px; height: 50px; border-radius: 100%;border: 2px solid #C47222;">
+        <span
+          style="display: block;color: #C47222;font-weight: bold;font-size: 120%;">{{ pagedata[2].userAO.userName
+          }}</span>
+      </div>
+
+    </div>
+
+    <div>
+      <img :src="imgOrigin+  'biz/1657789683976_d8ce47d3156c437f8ab54e2008daf398.png'" alt="" style="width: 100%;">
+    </div>
+
+    <div v-for="(item,index) in pagedata" v-if="index>2" :key="index" class="list" @click="toUser(item)">
+      <span style="width: 28px;text-align: center;">{{ item.rank }}</span>
+      <img :src="imgOrigin+item.userAO.icon + '?x-oss-process=image/resize,h_80/format,webp/quality,q_75'" alt=""
+           style="width: 40px; height: 40px; border-radius: 100%;margin: 0 5px;">
+      <span style="flex: 1;">{{ item.userAO.userName }}</span>
+      <span style="width: 60px;text-align: right;">{{ item.rating }}</span>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import Header from '@/components/common/Header.vue'
-import * as api from '@/api/api'
+import * as api from "@/api/api";
+
 export default {
-  name: 'tuxunRank',
-  components: {
-    
+  name: "tuxunRank",
+  components: {},
+  data() {
+    return {
+      pagedata: [],
+      timer: 0
+    };
   },
-  data(){
-      return {
-          pagedata: [],
-        timer: 0
-      }
+  created() {
   },
-  created(){
-  },
-  mounted(){
-    this.getButtonRank()
+  mounted() {
+    this.getButtonRank();
     document.title = "图寻总积分";
 
     let _this = this;
-    let timer = setInterval(()=>{
-      _this.getButtonRank()
-    },5000)
+    setInterval(() => {
+      _this.getButtonRank();
+    }, 5000);
   },
-  methods:{
-    getButtonRank(){
-        api.getByPath('/api/v0/tuxun/getRank').then(res=>{
-            this.pagedata = res.data
-        })
+  methods: {
+    getButtonRank() {
+      api.getByPath("/api/v0/tuxun/getRank").then(res => {
+        this.pagedata = res.data;
+      });
     },
-    toUser(item){
-      window.open(location.origin + '/tuxun/user/'+item.userAO.userId,"_blank");
-    },
-  },
-}
+    toUser(item) {
+      window.open(location.origin + "/tuxun/user/" + item.userAO.userId, "_blank");
+    }
+  }
+};
 </script>
-<style scoped lang="scss">
-.container{
-    // height: ;
-    padding-top: 0;
-    height: 100vh;
-}
-.title{
-  font-size: 16px;
-  padding-right: 10px;
-}
-.content{
-    .item{
-        display: flex;
-        justify-content: space-between;
-        padding: 5px;
-        border-bottom: 1px solid #f1f1f1;
-        // height: 60px;
-        // box-sizing: border-box;
-        overflow: hidden;
-        text-align: center;
-        align-items: center;
+<style lang="scss" scoped>
+.list {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  margin: 2px 0;
+  padding: 2px 4px;
+  border-bottom: 1px solid #f9f9f9;
 
-        .left{
-            flex: 1;
-            display: flex;
-            img{
-                flex: 0 0 40px;
-                width: 40px;
-                height: 40px;
-                border-radius: 50%;
-                margin-right: 10px;
-                vertical-align: middle;
-            }
-            .title{
-                font-size: 16px;
-            }
-            .desc{
-                width: 180px;
-                white-space: nowrap;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                color: #888;
-                font-size: 13px;
-            }
-            .info{
-                display: flex;
-                flex-direction: column;
-                justify-content: space-around;
-                
-            }
-        }
-        .right{
-            flex: 0 0 60px;
-            // line-height: 40px;
-            text-align: center;
-            font-size: 12px;
-            p{
-                font-size: 14px;
-            }
-        }
-    }
+  &:hover {
+    background-color: #f6f6f6;
+  }
 }
-.nav{
-    padding: 20px;
-    img{
-        width: 24px;
-        height: 24px;
-        vertical-align: middle;
-        margin-right: 10px;
-    }
-    font-size: 18px;
+
+.podium {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
 }
 </style>
