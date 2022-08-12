0745c255c5ac8340c0c86a7116fe460c67e6a76d
cijianzy <cijianzy@gmail.com>
2022-08-12 15:14:53 +0800

    fix: 优化邀请Solo

--- src/views/tuxun/TXNewHome.vue
+++ src/views/tuxun/TXNewHome.vue
@@ -103,8 +103,8 @@ export default {
     createSolo() {
       api.getByPath("/api/v0/tuxun/solo/create").then(res => {
         var id = res.data.id;
-        window.open(location.origin + '/tuxun/solo_game?gameId=' + id,"_blank");
-        // window.location.href = '/tuxun/solo_game?gameId=' + id;
+        // window.open(location.origin + '/tuxun/solo_game?gameId=' + id,"_blank");
+        window.location.href = '/tuxun/solo_game?gameId=' + id;
         // this.$router.push({path: '/tuxun/solo_game?gameId=' + id}).catch((e) => {})
       });
     },
