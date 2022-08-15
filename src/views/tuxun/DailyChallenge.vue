f9e8d0d8ae775606a417ebe30e0dc596aa1eb7e1
cijianzy <cijianzy@gmail.com>
2022-08-15 21:12:55 +0800

    fix: 优化提示

--- src/views/tuxun/DailyChallenge.vue
+++ src/views/tuxun/DailyChallenge.vue
@@ -9,7 +9,7 @@
           {{this.getDate()}}
         </div>
         <div class="hint">
-          每日挑战所有人的题目都一样
+          每日0点更新，所有人的题目统一
         </div>
         <el-button v-if="(this.gameData && this.gameData.status === 'ready') || this.showBegin" type="primary" size="large" @click="begin">开始每日挑战</el-button>
         <el-button v-if="this.gameData && this.gameData.status === 'ongoing'" type="warning" size="large" @click="again">继续每日挑战</el-button>
