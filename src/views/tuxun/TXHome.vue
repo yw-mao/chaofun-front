15d62c47be27849ca2e36f8b723f92ca5f66d77a
cijianzy <cijianzy@gmail.com>
2022-08-29 17:40:51 +0800

    fix: 优化逻辑

--- src/views/tuxun/TXHome.vue
+++ src/views/tuxun/TXHome.vue
@@ -27,7 +27,7 @@
 
     <div id="container" :class="[{'im-view': !ISPHONE}, {'im-view-phone': ISPHONE}]">
       <div v-show="this.contentType === 'panorama' && this.baiduPano && this.baiduPano !== null"  id="panorama" style="width: 100%; height: 100%;"></div>
-      <div v-if="this.contentType === 'panorama' && !(this.baiduPano && this.baiduPano !== null) " id="viewer"  style="width: 100%; height: 100%"></div>
+      <div v-show="this.contentType === 'panorama' && !(this.baiduPano && this.baiduPano !== null) " id="viewer"  style="width: 100%; height: 100%"></div>
       <img v-if="this.image && this.contentType === 'image'" v-viewer="{inline: false}" :data-source="imgOrigin+ this.image" style=" width: 100%;height: 100%;object-fit: contain;"  :src="imgOrigin+ this.image" alt=""></img>
       <video style="height: 100%; max-width: 100%;"
              v-if="this.image && this.contentType === 'video'"
