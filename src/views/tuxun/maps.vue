5cebc4efb63a7e7c2379f865a80f033934049f72
cijianzy <cijianzy@gmail.com>
2022-09-01 16:13:49 +0800

    fix: 优化一下练习赛

--- src/views/tuxun/maps.vue
+++ src/views/tuxun/maps.vue
@@ -7,6 +7,40 @@
         练习赛
       </div>
       <section class="game_entrance">
+        <div class="first_session_head" v-if="recentPagedata && recentPagedata.length >= 1">最近访问</div>
+        <div class="line" v-if="recentPagedata && recentPagedata.length >= 1"></div>
+        <div class="grid_main" v-if="recentPagedata && recentPagedata.length >= 1">
+          <div v-for="(item, index) in recentPagedata" @click="toMaps(item)"  :style="{'background-image': 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.6)), url('+ imgOrigin + (item.cover ?? 'biz/1659323781589_7d19c33667a54a4dabb0405ee5aec20f.jpeg') + '?x-oss-process=image/resize,h_400)','background-size':'cover'}" class="card">
+            <div class="title">
+              {{item.name}}
+            </div>
+            <div class="describe">
+              {{item.desc}}
+            </div>
+            <div class="players">
+              玩家人次: {{item.players}}
+            </div>
+          </div>
+        </div>
+
+        <div class="session_head">最新发布</div>
+        <div class="line"></div>
+        <div class="grid_main">
+          <div v-for="(item, index) in newPagedata" @click="toMaps(item)"  :style="{'background-image': 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.6)), url('+ imgOrigin + (item.cover ?? 'biz/1659323781589_7d19c33667a54a4dabb0405ee5aec20f.jpeg') + '?x-oss-process=image/resize,h_400)','background-size':'cover'}" class="card">
+            <div class="title">
+              {{item.name}}
+            </div>
+            <div class="describe">
+              {{item.desc}}
+            </div>
+            <div class="players">
+              玩家人次: {{item.players}}
+            </div>
+          </div>
+        </div>
+
+        <div class="session_head" >热度排序</div>
+        <div class="line"></div>
         <div class="grid_main">
           <div v-for="(item, index) in pagedata" @click="toMaps(item)"  :style="{'background-image': 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.6)), url('+ imgOrigin + (item.cover ?? 'biz/1659323781589_7d19c33667a54a4dabb0405ee5aec20f.jpeg') + '?x-oss-process=image/resize,h_400)','background-size':'cover'}" class="card">
             <div class="title">
@@ -20,6 +54,8 @@
             </div>
           </div>
         </div>
+
+
       </section>
     </div>
 </template>
@@ -36,24 +72,40 @@ export default {
   data(){
     return {
       pagedata: [],
+      newPagedata: [],
+      recentPagedata: [],
       timer: 0
     }
   },
   created(){
   },
   mounted(){
-    this.getButtonRank()
-    document.title = "练习赛";
+    this.getHotMaps();
+    this.getNewMaps();
+    this.getRecentPageData();
+    document.title = "图寻-练习赛";
 
     let _this = this;
     let timer = setInterval(()=>{
-      _this.getButtonRank()
+      _this.getHotMaps();
+      _this.getNewMaps();
+      _this.getRecentPageData();
     },5000)
   },
   methods:{
-    getButtonRank(){
+    getHotMaps(){
       api.getByPath('/api/v0/tuxun/maps/list').then(res=>{
         this.pagedata = res.data
+      })
+    },
+    getNewMaps(){
+      api.getByPath('/api/v0/tuxun/maps/listNew').then(res=>{
+        this.newPagedata = res.data
+      })
+    },
+    getRecentPageData(){
+      api.getByPath('/api/v0/tuxun/maps/listRecent').then(res=>{
+        this.recentPagedata = res.data
       })
     },
     toMaps(item){
@@ -75,7 +127,7 @@ export default {
   background-color: #090723;
   .nav {
     color: white;
-    font-size: xx-large;
+    font-size: xxx-large;
     font-weight: bold;
     padding-top: 3rem;
   }
@@ -100,11 +152,30 @@ export default {
     margin-top: 3rem;
     margin-bottom: 5rem;
 
+    .line {
+      width: 100%;
+      height: 2px;
+      background-color: white;
+      margin-bottom: 2rem;
+      margin-top: 1rem;
+    }
+    .first_session_head {
+      font-size: x-large;
+      color: whitesmoke;
+      font-weight: bolder;
+    }
+    .session_head {
+      font-size: x-large;
+      color: whitesmoke;
+      padding-top: 2rem;
+      font-weight: bolder;
+    }
     .grid_main {
       display: grid;
       grid-row-gap: 1.5rem;
       grid-column-gap: 1.5rem;
       grid-template-columns: repeat(3, 1fr);
+
       .card {
         background-color: rgb(25,26,46, 0.9);
         background-position: 50%;
