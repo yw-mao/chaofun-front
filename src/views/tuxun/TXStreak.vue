cdf4bc2d9a5a4226b83d97cbb743674f4906656d
cijianzy <cijianzy@gmail.com>
2022-09-21 23:45:51 +0800

    fix: 排行榜支持

--- src/views/tuxun/TXStreak.vue
+++ src/views/tuxun/TXStreak.vue
@@ -16,10 +16,23 @@
   </div>
 
   <div class="rank">
-    排行榜(实现中)
+    排行榜
   </div>
-  <div style="color: white;">
-    (我知道你很急，但是你先别急)
+  <div class="rank_container" v-if="this.rank">
+    <div @click="toUser(item.user)" v-for="(item,index) in this.rank" :key="index" class="item">
+      <div class="left">
+        <div class="number">
+          {{index + 1}}.
+        </div>
+        <img style="object-fit: cover;" :src="imgOrigin+ item.user.icon + '?x-oss-process=image/resize,h_80/format,webp/quality,q_75'" alt="">
+        <div class="info">
+          <div class="title">{{item.user.userName}}</div>
+        </div>
+      </div>
+      <div class="right">
+        <p>连胜: {{item.bestStreaks}}</p>
+      </div>
+    </div>
   </div>
 
 </div>
@@ -30,6 +43,13 @@ import * as api from '../../api/api'
 
 export default {
   name: "TXStreak",
+  data(){
+    return {
+      rank: null,
+    }
+  },  mounted() {
+    this.listRank();
+  },
   methods: {
     goHome() {
       window.location.href = '/tuxun';
@@ -37,6 +57,12 @@ export default {
     createNew() {
       api.getByPath("/api/v0/tuxun/streak/createCountryStreak").then(res => {
         window.location.href = '/tuxun/streak_game?streakId=' + res.data.id;
+      });
+    },
+
+    listRank() {
+      api.getByPath("/api/v0/tuxun/streak/listRank", {type: 'country'}).then(res => {
+        this.rank = res.data
       });
     }
 
@@ -74,9 +100,90 @@ export default {
     font-size: large;
     color: darkgray;
   }
+  .rank_container {
+    padding-top: 2rem;
+    padding-bottom: 5rem;
+    margin: auto;
+    width: 40%;
+
+    .item {
+      display: flex;
+      justify-content: space-between;
+      padding: 5px;
+      border-bottom: 1px solid #f1f1f1;
+      // height: 60px;
+      // box-sizing: border-box;
+      overflow: hidden;
+      color: white;
+      .left {
+        flex: 1;
+        display: flex;
+
+        .number {
+          //position: absolute;
+          text-align: end;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100%;
+          font-size: 16px;
+          margin-right: 8px;
+        }
+        img {
+          flex: 0 0 40px;
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+          margin-right: 10px;
+          vertical-align: middle;
+        }
+
+        .title {
+          font-size: 16px;
+          color: white;
+        }
+
+        .desc {
+          width: 180px;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          color: #888;
+          font-size: 13px;
+        }
+
+        .info {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+          color: white;
+        }
+      }
+
+      .right {
+        display: flex;
+        flex: 0 0 60px;
+        // line-height: 40px;
+        text-align: center;
+        font-size: 12px;
+        flex-direction: column;
+        justify-content: space-around;
+
+        p {
+          font-size: 14px;
+        }
+      }
+    }
+  }
 }
 
 
-
+@media only screen and (max-width: 679px) {
+  .container {
+    .rank_container {
+      width: 90%;
+    }
+  }
+}
 
 </style>
