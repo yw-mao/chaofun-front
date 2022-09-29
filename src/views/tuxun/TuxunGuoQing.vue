9aea2df806a4af68843fbc0ee8bbc39bef5ecbe3
cijianzy <cijianzy@gmail.com>
2022-09-29 21:18:21 +0800

    fix: 国庆活动起头

---
+++ src/views/tuxun/TuxunGuoQing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TuxunGuoQing"
+}
+</script>
+
+<style lang="scss" scoped>
+
+.container {
+
+}
+
+</style>
