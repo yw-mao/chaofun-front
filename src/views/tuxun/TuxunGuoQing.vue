6570d173716a4fcf1c9c79e36c3a9548f7381bd2
cijianzy <cijianzy@gmail.com>
2022-09-30 11:36:14 +0800

    fix：国庆页面支持

--- src/views/tuxun/TuxunGuoQing.vue
+++ src/views/tuxun/TuxunGuoQing.vue
@@ -9,11 +9,11 @@
     <div class="desc">
       10月1日-10月7日，每天8点-23点，每整点一场. 按照最终存活场次排序，前10名获得积分奖励 1000，900依次递减的积分奖励
     </div>
-    <div v-if="">
-      <div>
+    <div v-if="nextStartTime" class="next-round">
+      <div class="next-round-info">
         下一场倒计时
       </div>
-      <div >
+      <div class="next-round-count">
         {{this.timeLeftStr}}
       </div>
     </div>
@@ -24,24 +24,46 @@
 </template>
 
 <script>
+
+import * as api from '../../api/api'
+
 export default {
   name: "TuxunGuoQing",
 
   data() {
     return {
       timeLeftStr: null,
-
-      loading: false,
-      options: []
+      nextStartTime: null,
+      gameId: null
     }
   },
 
   mounted() {
+    this.get();
+    this.countDown();
   },
 
   methods: {
     goHome() {
       window.location.href = '/tuxun';
+    },
+
+    countDown() {
+      this.time = setInterval(function () {
+        if (!this.nextStartTime) {
+          return;
+        }
+        var timeLeft = parseInt((this.nextStartTime - (new Date().getTime())) / 1000);
+        this.timeLeftStr = Math.floor(timeLeft / 60 / 60 ) + ' 小时 ' + Math.floor(timeLeft / 60 % 60).toString().padStart(2, '0') + ' 分 ' + (timeLeft % 60).toString().padStart(2, '0') + ' 秒';
+      }.bind(this), 1000)
+    },
+
+    get() {
+      this.nextStartTime = null;
+      api.getByPath("/api/v0/tuxun/br/get").then(res => {
+        this.gameId = res.data.gameId;
+        this.nextStartTime = res.data.nextStartTime;
+      });
     },
   }
 }
@@ -67,6 +89,22 @@ export default {
     font-size: xxx-large;
     font-weight: bold;
     padding-top: 3rem;
+  }
+  .desc {
+    padding-top: 1rem;
+    color: gray;
+  }
+  .next-round {
+    padding-top: 1rem;
+    .next-round-info {
+      color: white;
+      font-size: 24px;
+    }
+
+    .next-round-count {
+      color: yellow;
+      font-size: 36px;
+    }
   }
 }
 
