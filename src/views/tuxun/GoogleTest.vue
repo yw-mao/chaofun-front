62782a7bb60ee322cd6806a2449e4c0faa5817b1
cijianzy <cijianzy@gmail.com>
2022-10-15 02:04:58 +0800

    fix: 测试

--- src/views/tuxun/GoogleTest.vue
+++ src/views/tuxun/GoogleTest.vue
@@ -9,36 +9,37 @@ export default {
   name: "GoogleTest",
   mounted() {
     let recaptchaScript = document.createElement('script')
-    recaptchaScript.setAttribute('src', 'https://chaofun-test.oss-cn-hangzhou.aliyuncs.com/google/js-1.js??key=AIzaSyAZZWaODguEaDaHTt7IuJuQEqoJ_mQXzaQ')
-    // recaptchaScript.setAttribute('src', 'https://gac-geo.googlecnapps.cn/maps/api/js?key=AIzaSyAZZWaODguEaDaHTt7IuJuQEqoJ_mQXzaQ')
-    recaptchaScript.setAttribute('type', 'text/javascript')
+    recaptchaScript.setAttribute('src', 'https://chaofun-test.oss-cn-hangzhou.aliyuncs.com/google/js-1.js??key=AIzaSyDvx5IM2bbHnOb4sY-0aISwH8bAxH7ecS4')
+    // recaptchaScript.setAttribute('src', 'https://gac-geo.googlecnapps.cn/maps/api/js?key=AIzaSyDvx5IM2bbHnOb4sY-0aISwH8bAxH7ecS4')
     document.head.appendChild(recaptchaScript)
     setTimeout(() => {
-
       this.test();
     }, 2000)
   },
 
   methods: {
     test() {
+
       var panorama = new google.maps.StreetViewPanorama(
           document.getElementById("map"), {
-            fullscreenControl:false,
-            panControl:false,
+            fullscreenControl: false,
+            panControl: false,
             addressControl: false,
             imageDateControl: true,
-            motionTrackingControl:false,
-            streetViewControl:true
+            motionTrackingControl: false,
+            streetViewControl: true
           }
       );
 
-      panorama.setPano('Yz0xoYXr1b4BH_TiNNdzXA');
+
+      panorama.setPano('CAoSLEFGMVFpcE9XZnBHQ2s3MEtEc2p0aU1idklDRy1DaHJuSjlfay1CRy1IbXhw');
       panorama.setPov({
         heading: 270,
         pitch: 0,
       });
       panorama.setVisible(true);
-    }
+    },
+
   }
 }
 </script>
