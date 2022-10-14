06f3a527db163bdf45f464bcaefc37ca2b785ab9
cijianzy <cijianzy@gmail.com>
2022-10-14 13:21:52 +0800

    fix: 支持分享街景功能

--- src/views/tuxun/RandomStreetView.vue
+++ src/views/tuxun/RandomStreetView.vue
@@ -3,6 +3,7 @@
     <div class="back_home" >
       <el-button type="primary" @click="goHome" round>←返回首页</el-button>
       <el-button type="warning" @click="change"  round>换一个</el-button>
+      <el-button @click="shareLink" round>分享街景</el-button>
     </div>
     <div id="map" class="container" style=""></div>
   </div>
@@ -18,11 +19,13 @@ export default {
   data() {
     return {
       panorama: null,
+      currentPanoId: null,
+      sharePanoId: null,
     }
   },
   mounted() {
-    let recaptchaScript = document.createElement('script')
-    loadScript('https://chaofun-test.oss-cn-hangzhou.aliyuncs.com/google/js-1.js??key=AIzaSyAZZWaODguEaDaHTt7IuJuQEqoJ_mQXzaQ&callback=initialize&v=weekly&channel=2').then(() => {
+    this.sharePanoId = this.$route.query.pano;
+    loadScript('https://chaofun-test.oss-cn-hangzhou.aliyuncs.com/google/js-1.js?key=AIzaSyAZZWaODguEaDaHTt7IuJuQEqoJ_mQXzaQ&callback=initialize&v=weekly&channel=2').then(() => {
       this.test();
     })
   },
@@ -39,21 +42,42 @@ export default {
             streetViewControl:true
           }
       );
-      this.change()
+      if (this.sharePanoId) {
+        this.setPano(this.sharePanoId)
+      } else {
+        this.change()
+      }
     },
     goHome() {
       window.location.href = '/tuxun';
     },
+    setPano(panoId) {
+      this.panorama.setPano(panoId);
+      this.panorama.setPov({
+        heading: 270,
+        pitch: 0,
+      });
+      this.panorama.setVisible(true);
+    },
     change() {
+      if (this.sharePanoId) {
+        window.location.href = '/tuxun/random'
+        return;
+      }
       api.getByPath("/api/v0/tuxun/random", {mapsId: this.mapsId}).then(res => {
-        this.panorama.setPano(res.data);
-        this.panorama.setPov({
-          heading: 270,
-          pitch: 0,
-        });
-        this.panorama.setVisible(true);
+        this.currentPanoId = res.data;
+        this.setPano(res.data);
       })
-    }
+    },
+    shareLink() {
+      var input = document.createElement('input');
+      input.setAttribute('value', 'https://chao.fan/tuxun/random?pano='+ this.currentPanoId  + '&source=g');
+      document.body.appendChild(input);
+      input.select();
+      var result = document.execCommand('copy');
+      document.body.removeChild(input);
+      this.$toast("复制邀请地址成功");
+    },
   }
 }
 </script>
