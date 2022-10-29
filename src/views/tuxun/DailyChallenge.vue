c7465fc504791c2ef7826f9b1f80d2cbf1323e7a
cijianzy <cijianzy@gmail.com>
2022-10-29 18:33:21 +0800

    fix: 显示优化

--- src/views/tuxun/DailyChallenge.vue
+++ src/views/tuxun/DailyChallenge.vue
@@ -27,6 +27,8 @@
         <el-button v-if="(this.gameData && this.gameData.status === 'ready') || this.showBegin" type="primary" size="large" @click="begin" round>开始每日挑战</el-button>
         <el-button v-if="this.gameData && this.gameData.status === 'ongoing'" type="warning" size="large" @click="again" round>继续每日挑战</el-button>
         <div class="score" v-if="this.gameData && this.gameData.status === 'finish'">今日得分: {{this.gameData.player.totalScore}}</div>
+        <div class="score" v-if="this.gameData && this.gameData.status === 'finish' && this.dailyChallengeRank">排名: {{this.dailyChallengeRank}}</div>
+        <div class="score" v-if="this.gameData && this.gameData.status === 'finish' && this.dailyChallengePercent">超过：{{((1 - this.dailyChallengePercent) * 100).toFixed(2)}} % 选手</div>
         <div class="rank">
           今日挑战排名
         </div>
@@ -62,6 +64,8 @@ export default {
       challengeId: undefined,
       gameData: undefined,
       rank: undefined,
+      dailyChallengeRank: null,
+      dailyChallengePercent: null,
       showBegin: false,
       type: 'noMove',
 
@@ -77,12 +81,16 @@ export default {
       this.gameData = null;
       this.showBegin = false;
       this.challengeId = null;
+      this.dailyChallengeRank = null;
+      this.dailyChallengePercent = null;
       this.rank = null;
       api.getByPath('/api/v0/tuxun/challenge/getGameInfo', {'day': '1', type: this.type}).then(res => {
         console.log(res.data)
         if (res.success) {
           if (res.data) {
             this.gameData = res.data;
+            this.challengeId = res.data.challengeId;
+            this.getDailyChallengeRank()
           }
         } else {
           this.showBegin = true;
@@ -94,6 +102,12 @@ export default {
           this.challengeId = res.data;
           this.getRank();
         }
+      })
+    },
+    getDailyChallengeRank() {
+      api.getByPath('/api/v0/tuxun/challenge/getDailyChallengeRank', {'challengeId': this.challengeId, 'gameId': this.gameData.id}).then(res=>{
+        this.dailyChallengeRank = res.data.rank;
+        this.dailyChallengePercent = res.data.percent;
       })
     },
     goHome() {
