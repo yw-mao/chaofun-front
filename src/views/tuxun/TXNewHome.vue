d1db606cc263fc4fb9008ae7e8abf31d83c154fa
cijianzy <cijianzy@gmail.com>
2022-11-10 13:18:04 +0800

    fix: 图寻个人赛

--- src/views/tuxun/TXNewHome.vue
+++ src/views/tuxun/TXNewHome.vue
@@ -14,7 +14,7 @@
     </div>
 
     <div class="game">
-      <div class="info" @click="toFirstTournament">炒饭网络迷踪图寻第三届团队赛圆满结束！</div>
+      <div class="info" @click="toFirstTournament">炒饭第四届图寻个人赛开始啦，11月13号周日下午2点开赛，大家赶快来报名吧！！！</div>
       <div v-if="times" class="times" @click="toFirstTournament">图寻总轮次数：<span style="font-size: 18px">{{times}}</span></div>
       <section class="game_entrance">
         <div class="first_session_head">单人</div>
@@ -245,9 +245,9 @@ export default {
     },
     toFirstTournament() {
       try {
-        window.flutter_inappwebview.callHandler('toAppPost', {postId: '1217468'});
+        window.flutter_inappwebview.callHandler('toAppPost', {postId: '1218963'});
       } catch (e) {
-        window.open('/p/1217468', "_blank");
+        window.open('/p/1218963', "_blank");
       }
     }
   }
