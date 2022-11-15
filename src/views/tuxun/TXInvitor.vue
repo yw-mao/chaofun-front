ef140308c5ba16b652ac8b75b41a497f4d4cb2cd
cijianzy <cijianzy@gmail.com>
2022-11-15 12:21:39 +0800

    fix: 优化返回首页

--- src/views/tuxun/TXInvitor.vue
+++ src/views/tuxun/TXInvitor.vue
@@ -339,6 +339,7 @@
       </div>
 
       <div class="home">
+        <el-button v-if="gameData.type === 'challenge' || gameData.type === 'battle_royale'" size="mini" @click="goHome" round>返回首页</el-button>
         <el-button size="mini"  @click="toReport" round> 坏题反馈 </el-button>
         <el-button size="mini" v-if="gameData && !gameData.player && gameData.type != 'battle_royale'"  @click="sendEmoji=true" round> 发送表情 </el-button>
         <el-button size="mini" v-if="gameData && (gameData.type ==='country_streak' || gameData.type ==='province_streak')"  @click="skip" round> 换一题 <span>
