12287d6a8cf77e97eaf93f373965ac7bdebfefb7
cijianzy <cijianzy@gmail.com>
2022-12-08 00:28:13 +0800

    fix: 涨价通知

--- src/components/chaofan/common/vip/vip.vue
+++ src/components/chaofan/common/vip/vip.vue
@@ -38,8 +38,22 @@
           <div>注：24小时免费会员，可循环使用，请勿滥用</div>
           <div v-if="this.totalTryTimes !== null">图寻已经被白嫖了 {{this.totalTryTimes}} 次，救救孩子吧</div>
         </div>
-        <div v-if="choose==='month'">注：感谢探索者您的支持！</div>
-        <div v-if="choose==='year'">注：冲冲冲！探索者，去征服世界吧！！！</div>
+        <div v-if="choose==='month'">
+          <div>
+            注：感谢探索者您的支持！
+          </div>
+          <div>
+            12月13日0点会调整到9.99元
+          </div>
+        </div>
+        <div v-if="choose==='year'">
+          <div>
+            注：冲冲冲！探索者，去征服世界吧！！！
+          </div>
+          <div>
+            12月13日0点会调整到66.99元
+          </div>
+        </div>
 
         <div style="padding-top: 10px; font-weight: bold; font-size: 16px">
           会员权益
