820a5ac79a280d412c7b467cfd4fcbddc0da69d2
cijianzy <cijianzy@gmail.com>
2022-12-12 23:17:17 +0800

    fix: 取消依赖

--- src/tuxun.main.js
+++ src/tuxun.main.js
@@ -48,7 +48,6 @@ import VueScroller from 'vue-scroller'
 Vue.use(VueScroller)
 
 import './icons' // icon
-import './permission' // permission control
 import './utils/error-log' // error log
 import * as filters from './filters' // global filters
 import i18n from './lang'
